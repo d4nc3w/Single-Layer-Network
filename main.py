9b4674144c2d17f9a76636a891d8fb5b5f681249
s28113 <s28113@pjwstk.edu.pl>
2024-04-19 20:13:22 +0200

    Overlaying general structure of code

---
+++ main.py
@@ -0,0 +1,19 @@
+import csv
+import numpy as np
+
+lang_train = 'lang.train.csv'
+lang_test = 'lang.test.csv'
+
+def loadData(file):
+    vectors = []
+    labels = []
+    def simplifyText(text):
+        # Keep only letters A-Z and convert to lowercase
+
+def train(data, labels):
+    # Implement the training algorithm
+
+def test(data, labels):
+    # Implement the testing algorithm
+
+
